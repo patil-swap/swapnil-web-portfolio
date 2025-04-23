bc464606e17a16c43938c25e34a2fa7dc33c15ff
Swapnil Patil <patil.k.swapnil@gmail.com>
2025-04-23 12:03:43 -0230

    Updated HERO content links

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,7 +27,7 @@ export const HERO_CONTENT = {
   description:
     "I'm currently helping businesses bring their visions to life through interactive digital solutions.",
   resumeLinkText: "Download Resume",
-  resumeLink: "/Swapnil_Patil_Resume.pdf"
+  resumeLink: "/Swapnil Patil Resume.pdf"
 };
 
 export const PROJECTS = [
